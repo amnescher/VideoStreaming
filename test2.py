328bb9c5aa812aa85c8a59994822c442659f3b3a
AmnEscherCloud <a.sabet@eschercloud.ai>
2024-01-20 19:14:19 +0000

    bug model composition fixed

--- test2.py
+++ test2.py
@@ -22,8 +22,9 @@ app = FastAPI()
 @serve.deployment(route_prefix="/")
 @serve.ingress(app)
 class Inference:
-    def __init__(self, fastapi_deployment: DeploymentHandle):
-        self.fastapi_deployment = fastapi_deployment
+    def __init__(self, Classifier1: DeploymentHandle, Classifier2: DeploymentHandle,):
+        self.fastapi_deployment =  Classifier1
+        self.Classifier2 = Classifier2
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s - %(levelname)s - %(message)s",
@@ -38,6 +39,7 @@ class Inference:
         try:
             # returns a ray object that needs to be converted 
             result1 = await self.fastapi_deployment.remote(request)
+            result2 = await self.Classifier2.remote(request)
             return "success"
         except Exception as e:
             logging.error(f"Error in Inference service: {e}")
@@ -56,7 +58,7 @@ class Classifier1:
         )
         self.logger = logging.getLogger(__name__)
         self.logger.propagate = True
-        self.logger.info("Initializing model...")
+        self.logger.info("Initializing model 1 ...")
         self.model = models.resnet18(pretrained=True)
         self.model.eval()
         self.transform = transforms.Compose([
@@ -66,7 +68,7 @@ class Classifier1:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         
-        self.logger.info("Model initialized")
+        self.logger.info("Model 1 initialized")
     @serve.batch(max_batch_size=2)
     async def handle_batch(self, requests: List[Input]):
         inputs = []
@@ -82,7 +84,7 @@ class Classifier1:
             with torch.no_grad():
                 predictions = self.model(inputs)
                 _, predicted = torch.max(predictions, 1)
-            self.logger.info(f"Predictions: {predicted}")
+            self.logger.info(f"Predictions classifier 1: {predicted}")
             return [predicted.item() for predicted in predicted]
         except Exception as e:
             self.logger.error(f"Error during model prediction: {e}")
@@ -108,7 +110,7 @@ class Classifier2:
         )
         self.logger = logging.getLogger(__name__)
         self.logger.propagate = True
-        self.logger.info("Initializing model...")
+        self.logger.info("Initializing model 2...")
         self.model = models.resnet18(pretrained=True)
         self.model.eval()
         self.transform = transforms.Compose([
@@ -118,7 +120,7 @@ class Classifier2:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         
-        self.logger.info("Model initialized")
+        self.logger.info("Model 2 initialized")
     @serve.batch(max_batch_size=2)
     async def handle_batch(self, requests: List[Input]):
         inputs = []
@@ -134,7 +136,7 @@ class Classifier2:
             with torch.no_grad():
                 predictions = self.model(inputs)
                 _, predicted = torch.max(predictions, 1)
-            self.logger.info(f"Predictions: {predicted}")
+            self.logger.info(f"Predictions classifier 2: {predicted}")
             return [predicted.item() for predicted in predicted]
         except Exception as e:
             self.logger.error(f"Error during model prediction: {e}")
